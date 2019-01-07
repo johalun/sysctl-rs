d6af3c66b206c68733060fda87391b0df73f10ce
Johannes Lundberg <johalun0@gmail.com>
2019-01-07 16:23:04 +0000

    Update FreeBSD docs

--- docs/freebsd/implementors/core/fmt/trait.Display.js
+++ docs/freebsd/implementors/core/fmt/trait.Display.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["sysctl"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"sysctl/enum.SysctlError.html\" title=\"enum sysctl::SysctlError\">SysctlError</a>",];
+implementors["sysctl"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"sysctl/enum.SysctlError.html\" title=\"enum sysctl::SysctlError\">SysctlError</a>",synthetic:false,types:["sysctl::SysctlError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
--- docs/freebsd/implementors/failure/trait.Fail.js
+++ docs/freebsd/implementors/failure/trait.Fail.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["sysctl"] = ["impl Fail for <a class=\"enum\" href=\"sysctl/enum.SysctlError.html\" title=\"enum sysctl::SysctlError\">SysctlError</a>",];
+implementors["sysctl"] = [{text:"impl Fail for <a class=\"enum\" href=\"sysctl/enum.SysctlError.html\" title=\"enum sysctl::SysctlError\">SysctlError</a>",synthetic:false,types:["sysctl::SysctlError"]},];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
