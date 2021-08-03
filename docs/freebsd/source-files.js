b3e54db68426726114f8fb93d65f2e603b6e6ead
Johannes Lundberg <johalun0@gmail.com>
2021-08-03 09:52:25 -0700

    Remove docs from repo since they are now on docs.rs

--- docs/freebsd/source-files.js
+++
@@ -1,3 +0,0 @@
-var N = null;var sourcesIndex = {};
-sourcesIndex["sysctl"] = {"name":"","dirs":[{"name":"unix","files":["ctl.rs","ctl_iter.rs","funcs.rs","mod.rs"]}],"files":["consts.rs","ctl_error.rs","ctl_flags.rs","ctl_info.rs","ctl_type.rs","ctl_value.rs","lib.rs","temperature.rs","traits.rs"]};
-createSourceSidebar();
